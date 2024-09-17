036f8f0355fa04c44421dcdacdb1df57e35a0263
nnuuvv <github@nuv.direct>
2024-09-17 15:43:29 +0200

    setup catppuccin colorscheme

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,13 +23,15 @@ vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
+    spec = {
     -- import your plugins
-    { import = "plugins" },
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "catppuccin-mocha" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+        { import = "plugins" },
+    },
+    -- Configure any other settings here. See the documentation for more details.
+    -- colorscheme that will be used when installing plugins.
+    install = { colorscheme = { "catppuccin" } },
+    -- automatically check for plugin updates
+    checker = { enabled = true },
 })
+
+vim.cmd.colorscheme("catppuccin")
