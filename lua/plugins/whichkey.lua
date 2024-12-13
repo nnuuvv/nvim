28fb918db33cb378024c713cff22530769ec6e31
nnuuvv <github@nuv.direct>
2024-12-13 14:00:33 +0100

    fix whichkey spec being in wrong location

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -3,8 +3,15 @@ return {
     event = "VeryLazy",
     opts = {
         -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
+        spec = {
+            { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
+            { '<leader>d', group = '[D]ocument' },
+            { '<leader>r', group = '[R]ename' },
+            { '<leader>s', group = '[S]earch' },
+            { '<leader>w', group = '[W]orkspace' },
+            { '<leader>t', group = '[T]oggle' },
+            { '<leader>f', group = '[F]ile' },
+        },
     },
     keys = {
         {
@@ -15,12 +22,4 @@ return {
             desc = "Buffer Local Keymaps (which-key)",
         },
     },
-    spec = {
-        { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
-        { '<leader>d', group = '[D]ocument' },
-        { '<leader>r', group = '[R]ename' },
-        { '<leader>s', group = '[S]earch' },
-        { '<leader>w', group = '[W]orkspace' },
-        { '<leader>t', group = '[T]oggle' },
-    }
 }
