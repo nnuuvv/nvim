a99249c6dd4660f5016e6049168790cba2cb1828
nnuuvv <github@nuv.direct>
2025-02-06 10:21:01 +0100

    add friendly-snippets

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -11,15 +11,12 @@ return { -- Autocompletion
                 return 'make install_jsregexp'
             end)(),
             dependencies = {
-              -- `friendly-snippets` contains a variety of premade snippets.
-              --    See the README about individual language/framework/plugin snippets:
-              --    https://github.com/rafamadriz/friendly-snippets
-              -- {
-              --   'rafamadriz/friendly-snippets',
-              --   config = function()
-              --     require('luasnip.loaders.from_vscode').lazy_load()
-              --   end,
-              -- },
+                {
+                    'rafamadriz/friendly-snippets',
+                    config = function()
+                        require('luasnip.loaders.from_vscode').lazy_load()
+                    end,
+                },
             },
         },
         'saadparwaiz1/cmp_luasnip',
@@ -55,7 +52,7 @@ return { -- Autocompletion
                 --  end
                 --
                 -- <c-j> will move you to the the next(right) of each of the expansion locations.
-                -- <c-k> is similar, except moving you backwards. 
+                -- <c-k> is similar, except moving you backwards.
                 ['<C-j>'] = cmp.mapping(function()
                     if luasnip.expand_or_locally_jumpable() then
                         luasnip.expand_or_locally_jump()
