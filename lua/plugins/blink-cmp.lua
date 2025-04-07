4b2a74895894d028a1fc7eab843906a4cb22b587
nnuuvv <github@nuv.direct>
2025-04-07 09:33:33 +0200

    swap back to nvim-autopairs

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -17,12 +17,6 @@ return {
         -- 'enter' for enter to accept
         -- 'none' for no mappings
         --
-        -- All presets have the following mappings:
-        -- C-space: Open menu or open docs if already open
-        -- C-n/C-p or Up/Down: Select next/previous item
-        -- C-e: Hide menu
-        -- C-k: Toggle signature help (if signature.enabled = true)
-        --
         -- See :h blink-cmp-config-keymap for defining your own keymap
         keymap = {
             preset = "none",
@@ -52,7 +46,8 @@ return {
                         "lsp"
                     }
                 }
-            }
+            },
+            accept = { auto_brackets = { enabled = true }, },
         },
 
         -- Default list of enabled providers defined so that you can extend it
