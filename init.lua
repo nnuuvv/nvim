6b432d50a269ba8e5db61828c76ebbf28aca175d
nnuuvv <github@nuv.direct>
2025-04-10 20:05:21 +0200

    add git commit feature to proof of concept plugin manager

--- init.lua
+++ init.lua
@@ -31,11 +31,13 @@ vim.api.nvim_create_user_command(
             .. ";" .. table.concat({ "git", "submodule", "update", plugin_dir:format(repo[2]) }, " ")
             .. ";" .. table.concat({ "git", "add", plugin_dir:format(repo[2]) }, " ")
             .. ";" .. table.concat({ "git", "add", ".gitmodules" }, " ")
+            .. ";" .. table.concat({ "git", "commit", "-m", "Added plugin", repo[2], "as submodule" }, " ")
 
         vim.notify("Adding submodule...")
         vim.notify("Initializing submodule...")
         vim.notify("Updating submodule...")
         vim.notify("Adding to git...")
+        vim.notify("Committing...")
         vim.fn.jobstart(command, {
             on_exit = on_done,
             cwd = vim.fn.stdpath("config")
@@ -74,12 +76,14 @@ vim.api.nvim_create_user_command(
             .. ";" .. table.concat({ "sed", "-i", "/" .. repo[1] .. "/d", ".gitmodules" }, " ")
             .. ";" .. table.concat({ "git", "add", plugin_dir:format(repo[1]) }, " ")
             .. ";" .. table.concat({ "git", "add", ".gitmodules" }, " ")
+            .. ";" .. table.concat({ "git", "commit", "-m", "Removed plugin", repo[2], "submodule" }, " ")
 
         vim.notify("Deinitting submodule...")
         vim.notify("Removing submodule dir from git...")
         vim.notify("Removing submodule folder...")
         vim.notify("Removing .gitmodules entry...")
         vim.notify("Updating git...")
+        vim.notify("Committing...")
         vim.fn.jobstart(command, {
             on_exit = on_done,
             cwd = vim.fn.stdpath("config")
@@ -91,9 +95,10 @@ vim.api.nvim_create_user_command(
 vim.api.nvim_create_user_command(
     'UpdatePlugins',
     function()
-        local command = 
-        table.concat({ "git", "submodule", "update" }, " ")
-        .. ";" .. table.concat({ "git", "add", "./.gitmodules" }, " ")
+        local command =
+            table.concat({ "git", "submodule", "update" }, " ")
+            .. ";" .. table.concat({ "git", "add", "./.gitmodules" }, " ")
+            .. ";" .. table.concat({ "git", "commit", "-m", "Updated plugin submodules" }, " ")
 
         local on_done = function()
             vim.cmd('packloadall! | helptags ALL')
